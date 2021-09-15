83b8c4095ac8a0238af2e34347cc41e8b89da0c4
KoJaem <woalswhwh@naver.com>
2021-09-15 23:56:26 +0900

    Box1 Repeat 추가

--- src/components/Box1.jsx
+++ src/components/Box1.jsx
@@ -7,14 +7,25 @@ export default function Box1() {
         <motion.div
             className="box"
             animate={{rotate:360}}
-            transition={{duration:1}}
+            transition={{
+                duration:2,
+                repeat:Infinity,
+                repeatType:"loop",
+            }}
+            
             >
 
         </motion.div>
         <motion.div>
             <svg xmlns="http://www.w3.org/2000/svg" width="750px" height="450px"
             viewBox="-150 0 600 150">
-                <motion.circle cx="0" cy="0"r="100" animate={{ cx: [null,300] }}/>
+                <motion.circle cx="0" cy="0"r="100" animate={{ cx: [null,300] }}
+                transition={{
+                    duration:2,
+                    repeat:Infinity,
+                    repeatType:'reverse',
+                    repeatDelay:1,
+                }}/>
                 </svg> 
         </motion.div>
         </div>
