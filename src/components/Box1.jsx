ee526b8fa24c64eda356635ae1f3b38cc703983e
KoJaem <woalswhwh@naver.com>
2021-09-16 00:43:47 +0900

    Box1 (Circle) 애니메이션 효과(ease), from 추가

--- src/components/Box1.jsx
+++ src/components/Box1.jsx
@@ -18,13 +18,16 @@ export default function Box1() {
         </motion.div>
         <motion.div>
             <svg xmlns="http://www.w3.org/2000/svg" width="750px" height="450px"
-            viewBox="-150 0 600 150">
-                <motion.circle cx="0" cy="0"r="100" animate={{ cx: [null,300] }}
+            viewBox="-150 0 800 150">
+                <motion.circle cx="0" cy="0"r="100" animate={{ cx: [null,500] }}
                 transition={{
                     duration:2,
                     repeat:Infinity,
                     repeatType:'reverse',
                     repeatDelay:1,
+                    ease: 'circInOut', // 애니메이션 효과
+                    from : -50, // 어디부터 시작할건지 (값 바꿔보면 바로 이해감)
+
                 }}/>
                 </svg> 
         </motion.div>
